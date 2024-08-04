a5ef4c85d28224f5483c4b3d1fe4291c28ee4fd6
Carlos Bracho <brachocarlos152@gmail.com>
2024-08-04 16:37:40 -0400

    added new menu for mobile version

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-icon').addEventListener('click', function () {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
+  });
